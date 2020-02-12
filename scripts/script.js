5a0d0c620efbf31f58864661a70945e211893389
DamianMadronal <99051248@mydavinci.nl>
2020-02-12 14:17:36 +0100

    set up foundation

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function start() {
+    var container = document.getElementById("stemwijzer-container");
+    container.classList.add("d-none");
+
+    var container = document.getElementById("questions-container");
+    container.classList.remove("d-none");
+}
