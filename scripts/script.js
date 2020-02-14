2110deb048ffb2a75a6f5c4e1335ffd87dd3b243
DamianMadronal <99051248@mydavinci.nl>
2020-02-14 13:51:04 +0100

    f

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,53 @@
+var count = 0;
+
+var answers = [];
+
 function start() {
+    // makeCheckboxes();
     var container = document.getElementById("stemwijzer-container");
     container.classList.add("d-none");
-
     var container = document.getElementById("questions-container");
     container.classList.remove("d-none");
+    getQuestion();
+}
+
+console.log(subjects.statement);
+function getQuestion(answer) {
+    for (i = 0; i <= count; i++) {
+        if (!subjects[count]) {
+            var container = document.getElementById("questions-container");
+            container.classList.add("d-none");
+
+            var container = document.getElementById("result-container");
+            container.classList.remove("d-none");
+            return;
+        }
+        if (subjects[count].statement) {
+            var question = subjects[count].statement;
+            var title = subjects[count].title;
+            count++;
+            document.getElementById("questionBox").innerHTML = question;
+            document.getElementById("questionTitle").innerHTML =
+                count + ". " + title;
+            count--;
+        }
+    }
+
+    if (count > 0) {
+        saveAnswer(answer, subjects[count].statement);
+    }
+
+    count++;
+}
+
+function saveAnswer(answer, question) {
+    var test = [answer, question];
+    answers.push(test);
+    console.log(answers);
+
+    getResult();
+}
+
+function getResult() {
+    for(a = 0; a )
 }
